389e2df8744d6b8d39bf0b26635f09a1548a2530
giulio <giuliomollo90@gmail.com>
2023-07-05 21:28:25 +0200

    first-test

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,14 @@
+import logo from '../../carpic.jpg';
+import '../../App.css';
+
+const Home = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default Home;
